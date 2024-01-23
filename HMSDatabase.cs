89ace16348a5882386eb31e95ba1b545678d3992
Richard Bodén <utjis@hotmail.com>
2024-01-23 12:06:21 +0100

    Lots of edits

---
+++ HMSDatabase.cs
@@ -0,0 +1,24 @@
+using System.Data;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System.Collections.Generic;
+
+namespace HMSDatabase
+{
+    public static class HMSDatabase
+    {
+        private static string connectionString = "server=localhost;user=root;database=hms";
+
+        public static List<dynamic> GetAllGuest()
+        {
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                string sql = "SELECT Guest.id, guest.Name as Name, Guest.Adress as Adress, Guest.Nationality as Nationality, Guest.PhoneNr as PhoneNr FROM guest";
+                var allGuest = db.Query<dynamic>(sql).ToList();
+                return allGuest;
+            }
+        }
+
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,37 +1,70 @@
 ﻿using System;
+using HMSDatabase;
 using System.Data;
 using MySql.Data.MySqlClient;
 using Dapper;
-class Program{
 
-    static void Main(string[] args)
-    {
-        string connectionString = "server=localhost;user=root;database=hotel";
 
-        using (IDbConnection db = new MySqlConnection(connectionString))
+
+namespace HMSApplication
+{
+    public class Program{
+
+        static void Main(string[] args)
         {
-            var result = db.Query("SELECT * FROM Guest");
-            foreach (var row in result)
+                bool isRunning = true;
+                while(isRunning == true)
+                {
+                    System.Console.WriteLine("--------------------------------");
+                    System.Console.WriteLine("| [1] Show Guests              |");
+                    System.Console.WriteLine("| [2] Show available rooms     |");
+                    System.Console.WriteLine("| [3] Add something            |");
+                    System.Console.WriteLine("| [4] Remove something         |");
+                    System.Console.WriteLine("| [5] Avlsuta programmet       |");
+                    System.Console.WriteLine("--------------------------------");
+                    System.Console.Write("Val: ");
+                    int menu = int.Parse(Console.ReadLine());
+
+                    switch (menu)
+                    {
+                        case 1:
+                            PrintAllGuests();
+                            break;
+                        case 2:
+                            
+                            break;
+                        case 3:
+                            
+                            break;
+                        case 4:
+                            
+                            break;
+                        case 5:
+                            System.Console.WriteLine("Avslutar..");
+                            isRunning = false;
+                            break;
+                        
+                        default:
+                            System.Console.WriteLine("Var vänlig välj ett annat alternativ");
+                            break;
+                    }
+                }
+            
+        }
+        static void PrintAllGuests()
+        {
+            System.Console.WriteLine("===================");
+            System.Console.WriteLine("Print Guest");
+            
+            List<dynamic> Guests = HMSDatabase.HMSDatabase.GetAllGuest();
+
+            
+            foreach (dynamic d in Guests)
             {
-                Console.WriteLine(row.Name);
+                
+                Console.WriteLine("Namn: " + d.Name + ", Nationalitet: " + d.Nationality + " Adress: " + d.Adress  + ", DateOfBirth: " + d.DateOfBirth + "Phone NR: " + d.PhoneNr);
             }
         }
-    }
-    static void PrintAllDogs()
-    {
-        System.Console.WriteLine("===================");
-        System.Console.WriteLine("Print Bookings");
-        
-        List<dynamic> doglist = DogDatabase.GetAllDogs();
 
-        // Loopa igenom listan
-        foreach (dynamic d in doglist)
-        {
-            // Skriv ut. Notera att egenskaperna (name, race, owner) måste stämma överens med result setet som vi får av dapper i
-            // metoden DogDatabase.GetAllDogs()
-            Console.WriteLine("Namn: " + d.name + ", ras: " + (string.IsNullOrEmpty(d.race) ? "okänd" : d.race)  + ", ägare:" + d.owner);
-        }
     }
-
 }
-
