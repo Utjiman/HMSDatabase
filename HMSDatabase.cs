54b2d8e31ee776f7741ca139638d9429c8cad6a7
Richard Bodén <utjis@hotmail.com>
2024-01-29 14:48:36 +0100

    Edited Connection string to not use using..

--- HMSDatabase.cs
+++ HMSDatabase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Data;
 using MySql.Data.MySqlClient;
 using Dapper;
@@ -7,43 +8,38 @@ namespace HMSDatabase
 {
     public static class HMSDatabase
     {
+        static IDbConnection dbConnection;
+
         // Connection string to connect to the MySQL database
-        private static string connectionString = "server=localhost;user=root;database=hms";
+        static string connectionString = "server=localhost;user=root;database=hms";
+
+        static HMSDatabase()
+        {
+            // Skapa en enda anslutning vid klassens instansiering
+            dbConnection = new MySqlConnection(connectionString);
+        }
 
         public static List<dynamic> GetAllGuest()
         {
-            // Establishes a connection to the database using the connection string.
-            // The 'using' statement ensures that the database connection is properly closed and disposed after use.
-            using (IDbConnection db = new MySqlConnection(connectionString))
-            {
-                
-                string sql = "SELECT Guest.id, guest.Name as Name, Guest.Adress as Adress, Guest.Nationality as Nationality, Guest.PhoneNr as PhoneNr FROM guest";
-                var allGuest = db.Query<dynamic>(sql).ToList();
-                return allGuest;
-            }
+            string sql = "SELECT Guest.id, guest.Name as Name, Guest.Adress as Adress, Guest.Nationality as Nationality, Guest.PhoneNr as PhoneNr FROM guest";
+            var allGuest = dbConnection.Query<dynamic>(sql).ToList();
+            return allGuest;
         }
 
         public static List<dynamic> GetAvailableRooms()
         {
-            using (IDbConnection db = new MySqlConnection(connectionString))
-            {
-                string sql = "SELECT * FROM Room WHERE RoomStatus = 'Available'"; 
-                var availableRooms = db.Query<dynamic>(sql).ToList();
-                return availableRooms;
-            }
+            string sql = "SELECT * FROM Room WHERE RoomStatus = 'Available'";
+            var availableRooms = dbConnection.Query<dynamic>(sql).ToList();
+            return availableRooms;
         }
-       public static void AddGuest(string guestName, DateTime dateOfBirth, string adress)
+
+        public static void AddGuest(string guestName, DateTime dateOfBirth, string adress)
         {
-            using (IDbConnection db = new MySqlConnection(connectionString))
-            {
-                string sql = "INSERT INTO Guest (Name, Adress, DateOfBirth) VALUES (@Name, @Adress, @DateOfBirth)";
-                db.Execute(sql, new { Name = guestName, Adress = adress, DateOfBirth = dateOfBirth }); // Executes the SQL command to insert a new guest into the database.
-            }
+            string sql = "INSERT INTO Guest (Name, Adress, DateOfBirth) VALUES (@Name, @Adress, @DateOfBirth)";
+            dbConnection.Execute(sql, new { Name = guestName, Adress = adress, DateOfBirth = dateOfBirth });
         }
 
+        
 
-
-
-       
     }
 }
--- obj/Debug/net8.0/hms.AssemblyInfo.cs
+++ obj/Debug/net8.0/hms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4daaf057236f7cb938ef1b795d450f1b2b1c03ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("hms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
